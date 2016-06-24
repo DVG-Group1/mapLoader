d15ca73385d06975397367035b857cde95a86835
Andrew Maxwell <andrew.maxwell@daugherty.com>
2016-06-24 12:54:15 -0500

    Minor changes

--- main.js
+++ main.js
@@ -13,18 +13,22 @@ const boundingBox = { // 70/270 Interchange
 	max: [-90.431133, 38.761202]
 };
 
+// comment out the above bounding box and uncomment the one below to widen the field of view to all of St. Louis
 // const boundingBox = { // All of STL
 // 	min: [-90.481630, 38.597452],
 // 	max: [-90.177094, 38.813553]
 // };
 
+const savedJSONLocation = 'public/coords.json';
+const saveDataAsJSON = processedData => {
+	const json = JSON.stringify(processedData);
+	return save(savedJSONLocation, json).then(() => console.log(`Saved ${json.length / 1e6}m`));
+};
+
 readJSON(jsonLocation).then(data => {
-
 	const {nodes, edges} = processData({excludeTypes, boundingBox, data});
+
 	return saveGraph({nodes, edges, neo4jUsername, neo4jPassword});
-
-	// this part here is for saving as JSON in the public folder so it can be viewed by public/index.html
-	// const json = JSON.stringify(processedData);
-	// return save('public/coords.json', json).then(() => console.log(`Saved ${json.length / 1e6}m`));
-
+	// if you'd like to save the nodes and edges as json, uncomment this line and comment out the one above
+	// return saveDataAsJSON({nodes, edges});
 }).catch(err => console.error(err));
